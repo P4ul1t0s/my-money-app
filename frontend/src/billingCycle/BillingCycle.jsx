0f033aaf54481de2775d9b88bf77813d514953c2
Paulo <paulomafra2004@gmail.com>
2023-01-22 17:23:12 -0300

    Componente TabHeader

--- frontend/src/billingCycle/BillingCycle.jsx
+++ frontend/src/billingCycle/BillingCycle.jsx
@@ -4,6 +4,7 @@ import Content from '../commom/template/Content.jsx'
 import Tabs from '../commom/tab/Tabs.jsx'
 import TabsHeader from '../commom/tab/TabsHeader.jsx'
 import TabsContent from '../commom/tab/TabsContent.jsx'
+import TabHeader from '../commom/tab/TabHeader.jsx'
 
 export default class BillingCycle extends Component{
     render(){
@@ -13,7 +14,10 @@ export default class BillingCycle extends Component{
                 <Content>
                     <Tabs>
                         <TabsHeader>
-                            AA
+                            <TabHeader label="Listar" icon="bars" target="tabList"/>
+                            <TabHeader label="Incluir" icon="plus" target="tabCreate"/>
+                            <TabHeader label="Alterar" icon="pencil" target="tabUpdate"/>
+                            <TabHeader label="Excluir" icon="trash-o" target="tabDelete"/>
                         </TabsHeader>
                         <TabsContent>
 
