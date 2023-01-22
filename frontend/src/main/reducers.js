7c732f528ed2a9165a19e377259fc7911ec95a56
Paulo <paulomafra2004@gmail.com>
2023-01-22 20:40:41 -0300

    Criando action e reducer de BillingCycle

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import tabReducer from '../commom/tab/tabReducer.js'
 import dashboardReducer from '../dashboard/dashboardReducer.js'
+import billingCycleReducer from '../billingCycle/billingCycleReducer.js'
 
 const rootReducers = combineReducers({
     dashboard: dashboardReducer,
-    tab: tabReducer
+    tab: tabReducer,
+    billingCycle: billingCycleReducer
 })
 
 export default rootReducers
