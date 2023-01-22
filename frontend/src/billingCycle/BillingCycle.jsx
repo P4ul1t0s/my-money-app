145f74b774f2742ee1df99ad694c56b9181f7f42
Paulo <paulomafra2004@gmail.com>
2023-01-22 16:03:53 -0300

    Refatorando BillingCycle.jsx

--- frontend/src/billingCycle/BillingCycle.jsx
+++ frontend/src/billingCycle/BillingCycle.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ContentHeader from '../commom/template/ContentHeader.jsx'
+import Content from '../commom/template/Content.jsx'
 
-export default props => (
-    <div>
-        <h1>Ciclo de Pagamento</h1>
-    </div>
-)
+export default class BillingCycle extends Component{
+    render(){
+        return(
+            <div>
+                <ContentHeader title="Cicles de Pagamento" small="Cadastro"/>
+                <Content>
+                    Ciclos de Pagamento
+                </Content>
+            </div>
+        )
+    }
+}
--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route, Router, Redirect, hashHistory} from 'react-router'
 import BillingCycle from '../billingCycle/BillingCycle.jsx'
-import Dashboard2 from '../dashBoard2/Dashboard2.jsx'
+// import Dashboard from '../dashBoard/Dashboard.jsx' // Dasshboard *com* Redux
+import Dashboard from '../dashBoard2/Dashboard2.jsx' // Dasshboard *sem* Redux
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path={'/'} component={Dashboard2}/>
+        <Route path={'/'} component={Dashboard}/>
         <Route path={'/billingCycles'} component={BillingCycle}/>
         <Redirect from='*' to='/'/>
     </Router>
