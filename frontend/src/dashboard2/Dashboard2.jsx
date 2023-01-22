bb59d42c56257160b62f6b53c14860f8c3f053df
Paulo <paulomafra2004@gmail.com>
2023-01-22 15:54:28 -0300

    Exemplo da Dashboard sem Redux

---
+++ frontend/src/dashboard2/Dashboard2.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import Content from '../commom/template/Content.jsx'
+import ContentHeader from '../commom/template/ContentHeader.jsx'
+import ValueBox from '../commom/widget/ValueBox.jsx'
+import axios from "axios";
+const BASE_URL = 'http://localhost:3003/api'
+
+export default class Dashboard2 extends Component{
+    constructor(props){
+        super(props)
+        this.state = {credit: 0, debt: 0}
+    }
+
+    componentWillMount(){
+        axios.get(`${BASE_URL}/billingCycles/summary`)
+        .then(resp => this.setState(resp.data))
+    }
+
+    render(){
+        const {credit, debt} = this.state
+
+        return(
+            <div>
+                <ContentHeader title="Dashboard" small="Versão 2.0"/>
+                <Content>
+                    <ValueBox cols='12 4' color='green' icon='bank' value={`R$ ${credit}`} text='Total de Crédito'/>
+                    <ValueBox cols='12 4' color='red' icon='credit-card' value={`R$ ${debt}`} text='Total de Débito'/>
+                    <ValueBox cols='12 4' color='purple' icon='money' value={`R$ ${credit - debt}`} text='Valor Consolidado'/>
+                </Content>
+            </div>
+        )
+    }
+}
--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Route, Router, Redirect, hashHistory} from 'react-router'
 import BillingCycle from '../billingCycle/BillingCycle.jsx'
-import Dashboard from '../dashBoard/Dashboard.jsx'
+import Dashboard2 from '../dashBoard2/Dashboard2.jsx'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path={'/'} component={Dashboard}/>
+        <Route path={'/'} component={Dashboard2}/>
         <Route path={'/billingCycles'} component={BillingCycle}/>
         <Redirect from='*' to='/'/>
     </Router>
