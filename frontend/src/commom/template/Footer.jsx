8b249247032df930b9ecbc80c314e2f5c3689222
Paulo <paulomafra2004@gmail.com>
2023-01-22 11:12:30 -0300

    Componente Rodapé

---
+++ frontend/src/commom/template/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2023
+            <a href="https://github.com/P4ul1t0s" target="_blank"> P4ul1t0s</a>
+        </strong>
+    </footer>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../commom/template/dependencies'
+import Footer from '../commom/template/Footer.jsx'
 import Header from '../commom/template/Header.jsx'
 import SideBar from '../commom/template/SideBar.jsx'
 
@@ -7,5 +8,9 @@ export default props => (
     <div className='wrapper'>
         <Header/>
         <SideBar/>
+        <div className='content-wrapper'>
+            <h1>Conteúdo</h1>
+        </div>
+        <Footer/>
     </div>
 )
