7635cc89e9c46efc3f18fb5cc6a1c478e75604d0
Paulo <paulomafra2004@gmail.com>
2023-01-23 21:52:59 -0300

    Configurando limpeza e troca de formulário

--- frontend/src/main/index.jsx
+++ frontend/src/main/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 import promise from 'redux-promise'
 import App from './app.jsx'
 import reducers from './reducers.js'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store}>
